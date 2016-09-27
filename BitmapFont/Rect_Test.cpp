43a98dd730117506289ed7239e75d3b2215859ff
Julien Richard <jairb@DESKTOP-6DHPMND>
2016-09-27 22:46:56 +0200

    Again

--- BitmapFont/Rect_Test.cpp
+++ BitmapFont/Rect_Test.cpp
@@ -6,81 +6,71 @@ namespace bmf
 {
 	TEST_CASE("Rect works properly", "[BitmapFontCache]")
 	{
-		SECTION("Create rect")
-		{
-			Rect newRect(0, 0, 1024, 512);
-			REQUIRE(newRect.width() == 1024);
-			REQUIRE(newRect.height() == 512);
-			REQUIRE(newRect.left() == 0);
-			REQUIRE(newRect.top() == 0);
+		// SECTION("Create rect")
+		// {
+			// Rect newRect(0, 0, 1024, 512);
+			// REQUIRE(newRect.width() == 1024);
+			// REQUIRE(newRect.height() == 512);
+			// REQUIRE(newRect.left() == 0);
+			// REQUIRE(newRect.top() == 0);
 			
-			Rect newRect2;
-			REQUIRE(newRect2.width() == 0);
-			REQUIRE(newRect2.height() == 0);
-			REQUIRE(newRect2.left() == 0);
-			REQUIRE(newRect2.top() == 0);
+			// Rect newRect2;
+			// REQUIRE(newRect2.width() == 0);
+			// REQUIRE(newRect2.height() == 0);
+			// REQUIRE(newRect2.left() == 0);
+			// REQUIRE(newRect2.top() == 0);
 
-			Rect newRect3(-256, -256, 1024, 512);
-			REQUIRE(newRect3.width() == 1024);
-			REQUIRE(newRect3.height() == 512);
-			REQUIRE(newRect3.left() == -256);
-			REQUIRE(newRect3.top() == -256);
-		}
+			// Rect newRect3(-256, -256, 1024, 512);
+			// REQUIRE(newRect3.width() == 1024);
+			// REQUIRE(newRect3.height() == 512);
+			// REQUIRE(newRect3.left() == -256);
+			// REQUIRE(newRect3.top() == -256);
+		// }
 		
-		SECTION("surface")		
-			Rect newRect(0, 0, 1024, 1024);
-			
-			REQUIRE(newRect.surface() == (1024 * 1024));
+		// SECTION("surface")
+		// {
+			// Rect newRect(0, 0, 1024, 1024);
+			// REQUIRE(newRect.surface() == (1024 * 1024));
 
-			Rect newRect2;
-			REQUIRE(newRect2.surface() == 0);
-		}
+			// Rect newRect2;
+			// REQUIRE(newRect2.surface() == 0);
+		// }
 
+		// SECTION("Right / bottom")
+		// {
+			// Rect newRect(0, 0, 1024, 1024);
+			// REQUIRE(newRect.right() == 1024);
+			// REQUIRE(newRect.bottom() == 1024);
 
-		SECTION("surface")
-		{
-			Rect newRect(0, 0, 1024, 1024);
-			REQUIRE(newRect.surface() == (1024 * 1024));
+			// Rect newRect2;
+			// REQUIRE(newRect2.right() == 0);
+			// REQUIRE(newRect2.bottom() == 0);
 
-			Rect newRect2;
-			REQUIRE(newRect2.surface() == 0);
-		}
+			// Rect newRect3(-256, -256, 1024, 1024);
+			// REQUIRE(newRect3.right() == 768);
+			// REQUIRE(newRect3.bottom() == 768);
+		// }
 
-		SECTION("Right / bottom")
-		{
-			Rect newRect(0, 0, 1024, 1024);
-			REQUIRE(newRect.right() == 1024);
-			REQUIRE(newRect.bottom() == 1024);
+		// SECTION("isSmallerOrEqualThan")
+		// {
+			// Rect newRect1(-1, -1, 2, 2);
+			// REQUIRE(newRect1.isSmallerOrEqualThan(newRect1));
 
-			Rect newRect2;
-			REQUIRE(newRect2.right() == 0);
-			REQUIRE(newRect2.bottom() == 0);
+			// Rect newRect2(-4, -4, 8, 8);
+			// REQUIRE(newRect1.isSmallerOrEqualThan(newRect2));
+			// REQUIRE(!newRect2.isSmallerOrEqualThan(newRect1));
 
-			Rect newRect3(-256, -256, 1024, 1024);
-			REQUIRE(newRect3.right() == 768);
-			REQUIRE(newRect3.bottom() == 768);
-		}
+			// Rect newRect3(-4, -4, 4, 1);
+			// REQUIRE(!newRect1.isSmallerOrEqualThan(newRect3));
+		// }
 
-		SECTION("isSmallerOrEqualThan")
-		{
-			Rect newRect1(-1, -1, 2, 2);
-			REQUIRE(newRect1.isSmallerOrEqualThan(newRect1));
-
-			Rect newRect2(-4, -4, 8, 8);
-			REQUIRE(newRect1.isSmallerOrEqualThan(newRect2));
-			REQUIRE(!newRect2.isSmallerOrEqualThan(newRect1));
-
-			Rect newRect3(-4, -4, 4, 1);
-			REQUIRE(!newRect1.isSmallerOrEqualThan(newRect3));
-		}
-
-		SECTION("offset")
-		{
-			Rect newRect1(-1, -1, 2, 2);
-			newRect1.offset(5, 10);
-			REQUIRE(newRect1.left() == 4);
-			REQUIRE(newRect1.top() == 9);
-		}
+		// SECTION("offset")
+		// {
+			// Rect newRect1(-1, -1, 2, 2);
+			// newRect1.offset(5, 10);
+			// REQUIRE(newRect1.left() == 4);
+			// REQUIRE(newRect1.top() == 9);
+		// }
 
 		SECTION("intersectsWith")
 		{
